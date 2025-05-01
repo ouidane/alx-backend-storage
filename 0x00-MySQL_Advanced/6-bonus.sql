59be696760ae07db9c0089194c7995ae7869a866
ouidane <ouidanezakaria@gmail.com>
2025-05-01 20:39:33 +0100

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,25 @@
+-- Creates a stored procedure AddBonus that inserts a correction for a student,
+-- creating the project if it does not exist.
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    DECLARE project_id INT;
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1;
+    IF project_id IS NULL THEN
+        INSERT INTO projects(name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END$$
+
+DELIMITER ;
