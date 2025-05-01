c4f827603bb7a00ef819fc53e9b8a91fae7b852a
ouidane <ouidanezakaria@gmail.com>
2025-05-01 20:32:45 +0100

    task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Trigger to reset valid_email only when email is changed
+
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email_before_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
